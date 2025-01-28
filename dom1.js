602ddf989e2be6ccba8dec3b8e0050d8a564c77d
saketh <pujarisaketh@gmail.com>
2025-01-28 23:11:20 +0530

    batch2

--- dom1.js
+++ dom1.js
@@ -81,9 +81,25 @@
 let form = document.querySelector("form");
 let username = document.getElementById("uName");
 let password = document.getElementById("uPass");
+let check = document.getElementById("check");
+let show = document.getElementById("show");
 
 let gender = document.getElementsByName("gender");
 // console.log(gender);
+
+// ! hiding and showing the password
+check.addEventListener("click" , (event)=>{
+    // console.log(event.target.checked);
+    if(event.target.checked == true){
+        password.setAttribute("type" , "text");
+        show.innerText = "hide password";
+    }else{
+        password.setAttribute("type", "password");
+        show.innerText="show password";
+        
+    }
+
+})
 
 form.addEventListener("submit" , event=>{
     event.preventDefault();
